0d242c233909d0588b1083271013a8bec9b92f1f
Conor <cwilson0005@hotmail.co.uk>
2019-02-25 11:39:49 +0000

    working on css to get ui buttons to align correctly

--- src/components/GameFlow/GameUI.js
+++ src/components/GameFlow/GameUI.js
@@ -1,13 +1,47 @@
 import React from "react";
+import './CSS/GameUI.css';
+import High_Low_Logic from ""
 
 const GameUI = (props) => {
 
+  function handleHighClick(){
+
+  };
+
+  function handleLowClick(){
+
+  };
+
+  function handleFreezeClick(){
+
+  };
+
+  function handleNewClick(){
+
+  };
+
   return (
-    <div>
-    <h2>Controls here</h2>
-      <button type="button" onclick={(props.handinfo)}>
+    <div className="ui-container">
+      <div className="player-info">
+        <h4>Player #</h4>
+        <button type="button">
+          Home
+        </button>
+      </div>
+      <div className="buttons">
+      <button type="button" onclick={() => {handleHighClick()}}>
         Higher
       </button>
+      <button type="button" onclick={() => {handleLowClick()}}>
+        Lower
+      </button>
+      <button type="button" onclick={() => {handleFreezeClick()}}>
+        Freeze
+      </button>
+      <button type="button" onclick={() => {handleNewClick()}}>
+        New
+      </button>
+      </div>
     </div>
   );
 
