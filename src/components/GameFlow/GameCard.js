c235faa1d99eba761aff29a0ccec0ac5ab550b34
Stephen Rafferty <srafferty73@gmail.com>
2019-02-26 14:10:04 +0000

    help page logic done

--- src/components/GameFlow/GameCard.js
+++ src/components/GameFlow/GameCard.js
@@ -3,12 +3,10 @@ import './CSS/GameCard.css';
 
 const GameCard = (props) => (
   <div className="media-card">
-  <div className="card-image">
-  <img className="resize" src="images/playing-card-back.png"/>
-  {/* <img className="resize" src={props.image} alt="Playing Cards"/>*/}
-
-
-  </div>
+    <div className="card-image">
+      {/*<img className="resize" src="images/playing-card-back.png"/>*/}
+      <img className="resize" src={props.image} alt="Playing Cards"/>
+    </div>
   </div>
 );
 
